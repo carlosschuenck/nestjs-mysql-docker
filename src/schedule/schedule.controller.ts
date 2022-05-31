ad9c157c42cc15725571bb021415d5725726370f
Carlos Schuenck <carlosschuenck@hotmail.com>
2022-06-01 00:50:17 +0100

    Adding examples of api param

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -2,7 +2,7 @@ import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 import {ScheduleService} from "./schedule.service";
 import {ScheduleDTO} from "./dto/schedule.dto";
 import {AvailabityDTO} from "./dto/availabity.dto";
-import {ApiResponse, ApiTags} from "@nestjs/swagger";
+import {ApiParam, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {IntervalDTO} from "./dto/interval.dto";
 
 @ApiTags('Schedule')
@@ -24,6 +24,19 @@ export class ScheduleController {
         return await this.service.saveCandidateSchedule(scheduleDTO);
     }
 
+
+    @ApiParam({
+        name: 'candidateId',
+        required: true,
+        example: 'ec2de694-c538-4a9a-8840-d26e5eedf96d',
+        schema: {oneOf: [{type: 'string'}]}
+    })
+    @ApiParam({
+        name: 'interviewers',
+        required: true,
+        example: '18f59d10-b146-4540-84bc-1764697e5543, 98d0b396-d56b-4025-99fa-d2a53f6bf572',
+        schema: {oneOf: [{type: 'string'}]}
+    })
     @ApiResponse({type: AvailabityDTO})
     @Get('/availability/candidate/:candidateId/interviewers/:interviewers')
     async get(@Param('candidateId') candidateId: string, @Param('interviewers') interviewers: string): Promise<AvailabityDTO> {
